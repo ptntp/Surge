b162811755031211a75f6a0da76909e4a7d54390
chiupam <chiupam@126.com>
2023-01-28 19:45:44 +0800

    Update

--- scripts/javascripts/procuratorate.js
+++ scripts/javascripts/procuratorate.js
@@ -1,16 +1,52 @@
-const $ = new Env('上班打卡')
+/**
+ *
+ * 使用方法：打开打开小程序手动进行一次打卡即可。
+ *
+ * Surge's Moudule: https://raw.githubusercontent.com/chiupam/surge/main/Surge/Procuratorate.sgmodule
+ * BoxJs: https://raw.githubusercontent.com/chiupam/surge/main/boxjs/chiupam.boxjs.json
+ *
+ * hostname: zhcj.kmcgjcy.cn
+ *
+ * type: http-request
+ * regex: ^https?://zhcj\.kmcgjcy\.cn/AttendanceCard/SaveAttCheckinout$
+ * script-path: https://raw.githubusercontent.com/chiupam/surge/main/scripts/javascripts/procuratorate.js
+ * requires-body: 1 | true
+ *
+ * type: cron
+ * cron: 1 58 8 * * 1,2,3,4,5
+ * script-path: https://raw.githubusercontent.com/chiupam/surge/main/scripts/javascripts/procuratorate.js
+ *
+ * type: cron
+ * cron: 1 1 17 * * 1,2,3,4,5
+ * script-path: https://raw.githubusercontent.com/chiupam/surge/main/scripts/javascripts/procuratorate.js
+
+ * =============== Surge ===============
+ * 工作打卡Cookie = type=http-request, pattern=^https?://zhcj\.kmcgjcy\.cn/AttendanceCard/SaveAttCheckinout$, requires-body=1, max-size=-1, script-path=https://raw.githubusercontent.com/chiupam/surge/main/scripts/javascripts/procuratorate.js, script-update-interval=0, timeout=10
+ * 上班打卡 = type=cron, cronexp="1 58 8 * * 1,2,3,4,5", wake-system=1, script-path=https://raw.githubusercontent.com/chiupam/surge/main/scripts/javascripts/procuratorate.js, script-update-interval=0, timeout=10
+ * 下班打卡 = type=cron, cronexp="1 1 17 * * 1,2,3,4,5", wake-system=1, script-path=https://raw.githubusercontent.com/chiupam/surge/main/scripts/javascripts/procuratorate.js, script-update-interval=0, timeout=10
+ *
+ */
+
+
+const time = new Date()
+const hours = time.getHours()
+const minutes = time.getMinutes()
+if (hours == 8) {
+  period = `上班打卡`
+} else if (hours == 17) {
+  period = `下班打卡`
+} else {
+  period = `打卡测试`
+}
+const $ = new Env(period)
 
 typeof $request !== `undefined` ? start() : main()
 
 function start() {
   if ($request.headers) {
-    if (!$.read(`procuratorate_cookie`)) {
-      $.write($request.headers.Cookie, `procuratorate_cookie`)
-      $.notice($.name, `✅ 首次使用 ✅`, `写入数据成功`, ``)
-    } else if ($request.headers.Cookie != $.read(`procuratorate_cookie`)) {
-      $.write($request.headers.Cookie, `procuratorate_cookie`)
-      $.notice($.name, `✅ 更新成功 ✅`, ``, ``)
-    }
+    $.write($request.headers.Cookie, `procuratorate_cookie`)
+    $.write($request.body, `procuratorate_body`)
+    $.notice($.name, `✅`, `写入数据成功`, ``)
   } else {
     $.notice($.name, `⭕ 无法读取请求头 ⭕`, `请检查配置是否正确`)
   }
@@ -18,21 +54,43 @@ function start() {
 }
 
 async function main() {
-  await index()
+  if (hours == 8 && minutes == 58) {
+    await index()
+  } else if (hours == 17 && minutes == 1) {
+    await index()
+  } else {
+    $.log(`不在打卡时间（精确到分钟）`)
+  }
   $.done()
 }
 
 function index() {
+  let checkin = $.toObj($.read(`procuratorate_body`))
+  let lng_substr = checkin.model.lng.substr(-4)
+  let lat_substr = checkin.model.lat.substr(-3)
+  let lng_random = Math.floor(Math.random() * 10000)
+  let lat_random = Math.floor(Math.random() * 1000)
+  checkin.model.lng = checkin.model.lng.replace(lng_substr, lng_random)
+  checkin.model.lat = checkin.model.lat.replace(lat_substr, lat_random)
   return new Promise(resolve => {
     const options = {
-      url: `https://zhcj.kmcgjcy.cn/AttendanceCard/Attendancecard?appid=103`,
-      headers: {"Cookie": $.read(`procuratorate_cookie`)}
+      url: `https://zhcj.kmcgjcy.cn/AttendanceCard/SaveAttCheckinout`,
+      headers: {
+        "Cookie": $.read(`procuratorate_cookie`),
+        "Accept": `application/json`
+      },
+      body: checkin
     }
-    $.log(`🧑‍💻 开始获取研究生账号信息`)
-    $.get(options, (error, response, data) => {
+    $.log(`🧑‍💻 开始${$.name}...`)
+    $.post(options, (error, response, data) => {
       if (data) {
-        data = $.toObj(data)
         $.log(data)
+        data = $.toObj(data)
+        if (data.success == `true`) {
+          $.notice($.name, `✅ 打卡成功 ✅`, data.message, ``)
+        } else {
+          $.notice($.name, `❌ 打卡失败 ❌`, data, ``)
+        }
       }
       resolve()
     })
@@ -48,7 +106,7 @@ function Env(name) {
     if (QX) return $prefs.valueForKey(key)
   }
   write = (key, val) => {
-    if (LN || SG) return $persistentStore.write(key, val); 
+    if (LN || SG) return $persistentStore.write(key, val);
     if (QX) return $prefs.setValueForKey(key, val)
   }
   notice = (title, subtitle, message, url) => {
@@ -56,21 +114,13 @@ function Env(name) {
     if (SG) $notification.post(title, subtitle, message, { url: url })
     if (QX) $notify(title, subtitle, message, { "open-url": url })
   }
-  get = (url, cb) => {
-    if (LN || SG) {$httpClient.get(url, cb)}
-    if (QX) {url.method = 'GET'; $task.fetch(url).then((resp) => cb(null, {}, resp.body))}
-  }
   post = (url, cb) => {
     if (LN || SG) {$httpClient.post(url, cb)}
     if (QX) {url.method = 'POST'; $task.fetch(url).then((resp) => cb(null, {}, resp.body))}
-  }
-  put = (url, cb) => {
-    if (LN || SG) {$httpClient.put(url, cb)}
-    if (QX) {url.method = 'PUT'; $task.fetch(url).then((resp) => cb(null, {}, resp.body))}
   }
   toObj = (str) => JSON.parse(str)
   toStr = (obj) => JSON.stringify(obj)
   log = (message) => console.log(message)
   done = (value = {}) => {$done(value)}
-  return { name, read, write, notice, get, post, put, toObj, toStr, log, done }
+  return { name, read, write, notice, post, toObj, toStr, log, done }
 }
